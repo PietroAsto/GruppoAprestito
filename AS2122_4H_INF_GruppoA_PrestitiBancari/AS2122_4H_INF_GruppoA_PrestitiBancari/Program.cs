e820b45463bf0c1d19096cd71c566dfb0f681595
P1PPY0NNA <filippo.veerginelli18@gmail.com>
2022-03-01 19:22:15 +0100

    prima push

---
+++ AS2122_4H_INF_GruppoA_PrestitiBancari/AS2122_4H_INF_GruppoA_PrestitiBancari/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AS2122_4H_INF_GruppoA_PrestitiBancari
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto di ingresso principale dell'applicazione.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Menu());
+        }
+    }
+}
