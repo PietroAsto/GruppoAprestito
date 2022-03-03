af882d4da38d69bcdb9e328d0b548438c8baa8a6
P1PPY0NNA <filippo.veerginelli18@gmail.com>
2022-03-03 12:05:33 +0100

    Aggiunto Textbox stipendio nella form ModificaCliente Codice per eliminare un cliente

--- AS2122_4H_INF_GruppoA_PrestitiBancari/AS2122_4H_INF_GruppoA_PrestitiBancari/Form/FormModificaCliente.Designer.cs
+++ AS2122_4H_INF_GruppoA_PrestitiBancari/AS2122_4H_INF_GruppoA_PrestitiBancari/Form/FormModificaCliente.Designer.cs
@@ -28,35 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btn_modifica_cliente = new System.Windows.Forms.Button();
+            this.btn_elimina_cliente = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.cb_scegli_cliente = new System.Windows.Forms.ComboBox();
+            this.tb_nome = new System.Windows.Forms.TextBox();
+            this.tb_cognome = new System.Windows.Forms.TextBox();
+            this.tb_cf = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tb_stipendio = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // btn_modifica_cliente
             // 
-            this.button1.Location = new System.Drawing.Point(29, 316);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Modifica ";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btn_modifica_cliente.Location = new System.Drawing.Point(29, 365);
+            this.btn_modifica_cliente.Name = "btn_modifica_cliente";
+            this.btn_modifica_cliente.Size = new System.Drawing.Size(75, 23);
+            this.btn_modifica_cliente.TabIndex = 0;
+            this.btn_modifica_cliente.Text = "Modifica ";
+            this.btn_modifica_cliente.UseVisualStyleBackColor = true;
+            this.btn_modifica_cliente.Click += new System.EventHandler(this.btn_modifica_cliente_Click);
             // 
-            // button2
+            // btn_elimina_cliente
             // 
-            this.button2.Location = new System.Drawing.Point(225, 316);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(86, 27);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Elimina";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btn_elimina_cliente.Location = new System.Drawing.Point(225, 361);
+            this.btn_elimina_cliente.Name = "btn_elimina_cliente";
+            this.btn_elimina_cliente.Size = new System.Drawing.Size(86, 27);
+            this.btn_elimina_cliente.TabIndex = 1;
+            this.btn_elimina_cliente.Text = "Elimina";
+            this.btn_elimina_cliente.UseVisualStyleBackColor = true;
+            this.btn_elimina_cliente.Click += new System.EventHandler(this.btn_elimina_cliente_Click);
             // 
             // label1
             // 
@@ -67,34 +71,35 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "Scegli Cliente ";
             // 
-            // comboBox1
+            // cb_scegli_cliente
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(181, 33);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(130, 24);
-            this.comboBox1.TabIndex = 3;
+            this.cb_scegli_cliente.FormattingEnabled = true;
+            this.cb_scegli_cliente.Location = new System.Drawing.Point(181, 33);
+            this.cb_scegli_cliente.Name = "cb_scegli_cliente";
+            this.cb_scegli_cliente.Size = new System.Drawing.Size(130, 24);
+            this.cb_scegli_cliente.TabIndex = 3;
+            this.cb_scegli_cliente.SelectedIndexChanged += new System.EventHandler(this.cb_scegli_cliente_SelectedIndexChanged);
             // 
-            // textBox1
+            // tb_nome
             // 
-            this.textBox1.Location = new System.Drawing.Point(181, 86);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(130, 22);
-            this.textBox1.TabIndex = 4;
+            this.tb_nome.Location = new System.Drawing.Point(181, 86);
+            this.tb_nome.Name = "tb_nome";
+            this.tb_nome.Size = new System.Drawing.Size(130, 22);
+            this.tb_nome.TabIndex = 4;
             // 
-            // textBox2
+            // tb_cognome
             // 
-            this.textBox2.Location = new System.Drawing.Point(181, 142);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(130, 22);
-            this.textBox2.TabIndex = 5;
+            this.tb_cognome.Location = new System.Drawing.Point(181, 142);
+            this.tb_cognome.Name = "tb_cognome";
+            this.tb_cognome.Size = new System.Drawing.Size(130, 22);
+            this.tb_cognome.TabIndex = 5;
             // 
-            // textBox3
+            // tb_cf
             // 
-            this.textBox3.Location = new System.Drawing.Point(181, 212);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(130, 22);
-            this.textBox3.TabIndex = 6;
+            this.tb_cf.Location = new System.Drawing.Point(181, 212);
+            this.tb_cf.Name = "tb_cf";
+            this.tb_cf.Size = new System.Drawing.Size(130, 22);
+            this.tb_cf.TabIndex = 6;
             // 
             // label2
             // 
@@ -123,24 +128,41 @@
             this.label4.TabIndex = 9;
             this.label4.Text = "Codice Fiscale";
             // 
-            // Modifica
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 284);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Stipendio";
+            // 
+            // tb_stipendio
+            // 
+            this.tb_stipendio.Location = new System.Drawing.Point(181, 278);
+            this.tb_stipendio.Name = "tb_stipendio";
+            this.tb_stipendio.Size = new System.Drawing.Size(130, 22);
+            this.tb_stipendio.TabIndex = 11;
+            // 
+            // FormModificaCliente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tb_stipendio);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.tb_cf);
+            this.Controls.Add(this.tb_cognome);
+            this.Controls.Add(this.tb_nome);
+            this.Controls.Add(this.cb_scegli_cliente);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Name = "Modifica";
+            this.Controls.Add(this.btn_elimina_cliente);
+            this.Controls.Add(this.btn_modifica_cliente);
+            this.Name = "FormModificaCliente";
             this.Text = "Modifica";
-            this.Load += new System.EventHandler(this.Modifica_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -148,15 +170,17 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btn_modifica_cliente;
+        private System.Windows.Forms.Button btn_elimina_cliente;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox cb_scegli_cliente;
+        private System.Windows.Forms.TextBox tb_nome;
+        private System.Windows.Forms.TextBox tb_cognome;
+        private System.Windows.Forms.TextBox tb_cf;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tb_stipendio;
     }
 }
--- AS2122_4H_INF_GruppoA_PrestitiBancari/AS2122_4H_INF_GruppoA_PrestitiBancari/Form/FormModificaCliente.cs
+++ AS2122_4H_INF_GruppoA_PrestitiBancari/AS2122_4H_INF_GruppoA_PrestitiBancari/Form/FormModificaCliente.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Prestiti_DLL;
 
 namespace AS2122_4H_INF_GruppoA_PrestitiBancari
 {
@@ -19,9 +20,32 @@ namespace AS2122_4H_INF_GruppoA_PrestitiBancari
             m1 = m;
         }
 
-        private void Modifica_Load(object sender, EventArgs e)
+        private void btn_modifica_cliente_Click(object sender, EventArgs e)
         {
+            
+        }
 
+        private void btn_elimina_cliente_Click(object sender, EventArgs e)
+        {
+            string nome_eliminato = tb_nome.Text;
+            string cognome_eliminato = tb_cognome.Text;
+            string cf_eliminato = tb_cf.Text;
+            double stipendio_eliminato = double.Parse(tb_stipendio.Text);
+            Cliente cliente = new Cliente(nome_eliminato, cognome_eliminato, cf_eliminato, stipendio_eliminato);
+
+            // Elimino un cliente
+            m1.banca.clienti.Remove(cliente);
+
+            // Svuoto TextBox
+            tb_nome.Text = "";
+            tb_cognome.Text = "";
+            tb_cf.Text = "";
+            tb_stipendio.Text = "";
+        }
+
+        private void cb_scegli_cliente_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //cb_scegli_cliente.Text = m1.banca.clienti[Cliente()]
         }
     }
 }
